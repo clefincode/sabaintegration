56f712d888ba4d6332e196bb5474c59e387cf3d5
Ahmad Kamal Eddin <ahmad@clefincode.com>
2023-02-27 13:48:11 +0000

    fix bug

--- sabaintegration/public/js/doctype/product_bundle.js
+++ sabaintegration/public/js/doctype/product_bundle.js
@@ -6,7 +6,7 @@ frappe.ui.form.on("Product Bundle", {
 		frm.toggle_enable("new_item_code", frm.is_new());
 		frm.set_query("new_item_code", () => {
 			return {
-				query: "sabaintegration.overrides.product_bundle_filters_for_new_item_code.get_new_item_code",
+				query: "sabaintegration.overrides.product_bundle.get_new_item_code",
                 // custom update ... new function created for filters
 			};
 		});
