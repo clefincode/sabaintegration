97746f463d0717b513f520fb4096c8774f465390
Ahmad Kamal Eddin <ahmad@clefincode.com>
2023-03-08 05:29:30 +0000

    cancel button, sort buttons and change font size

--- sabaintegration/public/js/doctype/opportunity.js
+++ sabaintegration/public/js/doctype/opportunity.js
@@ -1,15 +1,12 @@
 frappe.ui.form.on("Opportunity", {
     setup(frm) {
-        frm.all_options =  ['option_1', 'option_2', 'option_3', 'option_4', 'option_5', 'option_6', 'option_7', 'option_8', 'option_9', 'option_10']
+        frm.all_options =  ['option_1', 'option_2', 'option_3', 'option_4', 'option_5', 'option_6', 'option_7', 'option_8', 'option_9', 'option_10'];
+        frm.all_names =  ['name1', 'name2', 'name3', 'name4', 'name5', 'name6', 'name7', 'name8', 'name9', 'name10']
         frm.set_df_property('parent_items', 'cannot_add_rows', true);
 		frm.set_df_property('parent_items', 'cannot_delete_rows', true);
 
         frm.set_df_property('items', 'cannot_add_rows', true);
 		frm.set_df_property('items', 'cannot_delete_rows', true);
-
-        if (!frm.doc.parent_items || frm.doc.parent_items.length < 1) frm.toggle_display('parent_items', false);
-        if (!frm.is_new()) frm.doc.selected_option = 0
-        else if (frm.doc.with_items) frm.trigger("set_option")
         
         // show download button under items and packed items tables
         frm.get_docfield("items").allow_bulk_edit = 1;
@@ -23,22 +20,24 @@ frappe.ui.form.on("Opportunity", {
         
     },
     onload(frm){
-        if (frm.doc.items) frm.trigger("set_option")
+        if (frm.doc.items) frm.trigger("set_option");
         //for submitted option to stay read only after refresh
-        if  (frm.doc.option1status == "1") {frm.set_df_property('option_1', "read_only", 1);frm.set_df_property('option1_submit', 'hidden', 1);}
-        if  (frm.doc.option2status == "1") {frm.set_df_property('option_2', "read_only", 1);frm.set_df_property('option2_submit', 'hidden', 1);}
-        if  (frm.doc.option3status == "1") {frm.set_df_property('option_3', "read_only", 1);frm.set_df_property('option3_submit', 'hidden', 1);}
-        if  (frm.doc.option4status == "1") {frm.set_df_property('option_4', "read_only", 1);frm.set_df_property('option4_submit', 'hidden', 1);}
-        if  (frm.doc.option5status == "1") {frm.set_df_property('option_5', "read_only", 1);frm.set_df_property('option5_submit', 'hidden', 1);}
-        if  (frm.doc.option6status == "1") {frm.set_df_property('option_6', "read_only", 1);frm.set_df_property('option6_submit', 'hidden', 1);}
-        if  (frm.doc.option7status == "1") {frm.set_df_property('option_7', "read_only", 1);frm.set_df_property('option7_submit', 'hidden', 1);}
-        if  (frm.doc.option8status == "1") {frm.set_df_property('option_8', "read_only", 1);frm.set_df_property('option8_submit', 'hidden', 1);}
-        if  (frm.doc.option9status == "1") {frm.set_df_property('option_9', "read_only", 1);frm.set_df_property('option9_submit', 'hidden', 1);}
-        if  (frm.doc.option10status == "1") {frm.set_df_property('option_10', "read_only", 1);frm.set_df_property('option10_submit', 'hidden', 1);}
+        for (let i=0; i<frm.all_options.length - 1; i++){
+            let option = frm.all_options[i].replace('_','')
+            if (frm.doc[option + 'status'] == "1" ){
+                frm.set_df_property(frm.all_options[i], "read_only", 1);
+                frm.set_df_property(option + '_submit', 'hidden', 1);
+                // for cancel button showing for specific role users
+                if (frappe.user_roles.includes('0 CRM – Opportunity Option Cancellation')) {
+                    frm.set_df_property(option + '_cancel', 'hidden', 0);
+                }
+            }
+
+        }
         // testing submit button
         frm.trigger("scheck_option_status")
         frm.trigger("check_empty_option")
-        
+ 
     // testing submit button end
     },
     condition(frm) {
@@ -107,18 +106,12 @@ frappe.ui.form.on("Opportunity", {
         }
     },
     refresh(frm) {
-        frm.set_df_property('parent_items', 'cannot_add_rows', true);
-		frm.set_df_property('parent_items', 'cannot_delete_rows', true);
-
-        frm.set_df_property('items', 'cannot_add_rows', true);
-		frm.set_df_property('items', 'cannot_delete_rows', true);
-        // Custom Update
+        set_css(frm);
         frm.add_custom_button(__('Request For Quotation New Tab'),
             function() {
                 frm.trigger("make_request_for_quotation_new_tab")
             }, __('Create'));
         // $("a[data-label='Request%20For%20Quotation']").addClass("hidden");
-        //Custom Update end
         if (!frm.doc.parent_items || frm.doc.parent_items.length < 1) frm.toggle_display('parent_items', false);
         
         if (frm.is_new()) frm.doc.selected_option = 0; 
@@ -210,7 +203,10 @@ frappe.ui.form.on("Opportunity", {
     },
     check_empty_option: function(frm){
         for (let i=0; i<frm.all_options.length - 1; i++){
-            if (frm.fields_dict[frm.all_options[i]].grid.data && frm.fields_dict[frm.all_options[i]].grid.data.length != 0 ) frm.set_df_property(frm.all_options[i+1], 'hidden', 0);
+            if (frm.fields_dict[frm.all_options[i]].grid.data && frm.fields_dict[frm.all_options[i]].grid.data.length != 0 ){
+                frm.set_df_property(frm.all_options[i+1], 'hidden', 0);
+                frm.set_df_property(frm.all_names[i+1], 'hidden', 0);
+            } 
         }
     },
     scheck_option_status: function(frm){
@@ -218,6 +214,10 @@ frappe.ui.form.on("Opportunity", {
             let option_name = frm.all_options[i].replace('_','')
             if (!frm.fields_dict[frm.all_options[i]].grid.df.hidden) {
                 frm.set_df_property(option_name + '_submit', 'hidden', 0);
+                // Custom update for submit button to keep hidden if table submitted
+                if (frm.doc[option_name + 'status'] == "1" ){
+                    frm.set_df_property(option_name + '_submit', 'hidden', 1);
+                }// Custom update end
                 frm.set_df_property(option_name + '_copy', 'hidden', 0);
             }
             else {
@@ -293,10 +293,35 @@ frappe.ui.form.on("Opportunity", {
             'Are you sure you want to submit this option?',
             function(){
                 frm.set_df_property(option, "read_only", 1);
-                frm.set_value(option.replace('_','') + 'status', '1')
-                window.close();
+                frm.set_value(option.replace('_','') + 'status', '1');
+                refresh_field(option.replace('_','') + 'status');
+                
                 let option_btn_name = option.replace('_','');
                 frm.set_df_property(option_btn_name + '_submit', 'hidden', 1);
+                if (frappe.user_roles.includes('0 CRM – Opportunity Option Cancellation')) {
+                    frm.set_df_property(option_btn_name + '_cancel', 'hidden', 0);
+                }
+                window.close();
+                
+            },
+            function(){
+            }
+        )
+    },
+    cancel_option: function(frm ,option){
+        frappe.confirm(
+            'Are you sure you want to cancel this option?',
+            function(){
+                frm.set_df_property(option, "read_only", 0);
+                frm.set_value(option.replace('_','') + 'status', '0');
+                refresh_field(option.replace('_','') + 'status');
+                let option_btn_name = option.replace('_','');
+                // if (frappe.user_roles.includes('0 CRM – Opportunity Option Cancellation')) {
+                    
+                // }
+                frm.set_df_property(option_btn_name + '_cancel', 'hidden', 1);
+                frm.set_df_property(option_btn_name + '_submit', 'hidden', 0);
+                window.close();
             },
             function(){
             }
@@ -364,6 +389,36 @@ frappe.ui.form.on("Opportunity", {
     option10_copy(frm) {
         frm.events.copy_option(frm,"option_10");
     },
+    option1_cancel(frm) {
+        frm.events.cancel_option(frm,"option_1");
+    },
+    option2_cancel(frm) {
+        frm.events.cancel_option(frm,"option_2");
+    },
+    option3_cancel(frm) {
+        frm.events.cancel_option(frm,"option_3");
+    },
+    option4_cancel(frm) {
+        frm.events.cancel_option(frm,"option_4");
+    },
+    option5_cancel(frm) {
+        frm.events.cancel_option(frm,"option_5");
+    },
+    option6_cancel(frm) {
+        frm.events.cancel_option(frm,"option_6");
+    },
+    option7_cancel(frm) {
+        frm.events.cancel_option(frm,"option_7");
+    },
+    option8_cancel(frm) {
+        frm.events.cancel_option(frm,"option_8");
+    },
+    option9_cancel(frm) {
+        frm.events.cancel_option(frm,"option_9");
+    },
+    option10_cancel(frm) {
+        frm.events.cancel_option(frm,"option_10");
+    },
     copy_option: function(frm ,sourceoption){
         var emptyoption;
         var number ;
@@ -416,7 +471,8 @@ frappe.ui.form.on('Opportunity Option', {
         var child1 = locals[cdt][cdn];
         child1.qty = 1;
         refresh_field("option_1");
-        frm.set_df_property('option_2', 'hidden', 0)
+        frm.set_df_property('option_2', 'hidden', 0);
+        frm.set_df_property('name2', 'hidden', 0);
         frm.set_df_property('option1_submit', 'hidden', 0);
         frm.set_df_property('option1_copy', 'hidden', 0);
     },
@@ -432,7 +488,8 @@ frappe.ui.form.on('Opportunity Option', {
         var child2 = locals[cdt][cdn];
         child2.qty = 1;
         refresh_field("option_2");
-        frm.set_df_property('option_3', 'hidden', 0)
+        frm.set_df_property('option_3', 'hidden', 0);
+        frm.set_df_property('name3', 'hidden', 0);
         frm.set_df_property('option2_submit', 'hidden', 0);
         frm.set_df_property('option2_copy', 'hidden', 0);
        
@@ -441,7 +498,8 @@ frappe.ui.form.on('Opportunity Option', {
         var child3 = locals[cdt][cdn];
         child3.qty = 1;
         refresh_field("option_3");
-        frm.set_df_property('option_4', 'hidden', 0)
+        frm.set_df_property('option_4', 'hidden', 0);
+        frm.set_df_property('name4', 'hidden', 0);
         frm.set_df_property('option3_submit', 'hidden', 0);
         frm.set_df_property('option3_copy', 'hidden', 0);
 
@@ -451,7 +509,8 @@ frappe.ui.form.on('Opportunity Option', {
         var child4 = locals[cdt][cdn];
         child4.qty = 1;
         refresh_field("option_4");
-        frm.set_df_property('option_5', 'hidden', 0)
+        frm.set_df_property('option_5', 'hidden', 0);
+        frm.set_df_property('name5', 'hidden', 0);
         frm.set_df_property('option4_submit', 'hidden', 0);
         frm.set_df_property('option4_copy', 'hidden', 0);
 
@@ -461,7 +520,8 @@ frappe.ui.form.on('Opportunity Option', {
         var child5 = locals[cdt][cdn];
         child5.qty = 1;
         refresh_field("option_5");
-        frm.set_df_property('option_6', 'hidden', 0)
+        frm.set_df_property('option_6', 'hidden', 0);
+        frm.set_df_property('name6', 'hidden', 0);
         frm.set_df_property('option5_submit', 'hidden', 0);
         frm.set_df_property('option5_copy', 'hidden', 0);
 
@@ -471,7 +531,8 @@ frappe.ui.form.on('Opportunity Option', {
         var child6 = locals[cdt][cdn];
         child6.qty = 1;
         refresh_field("option_6");
-        frm.set_df_property('option_7', 'hidden', 0)
+        frm.set_df_property('option_7', 'hidden', 0);
+        frm.set_df_property('name7', 'hidden', 0);
         frm.set_df_property('option6_submit', 'hidden', 0);
         frm.set_df_property('option6_copy', 'hidden', 0);
 
@@ -481,7 +542,8 @@ frappe.ui.form.on('Opportunity Option', {
         var child7 = locals[cdt][cdn];
         child7.qty = 1;
         refresh_field("option_7");
-        frm.set_df_property('option_8', 'hidden', 0)
+        frm.set_df_property('option_8', 'hidden', 0);
+        frm.set_df_property('name8', 'hidden', 0);
         frm.set_df_property('option7_submit', 'hidden', 0);
         frm.set_df_property('option7_copy', 'hidden', 0);
 
@@ -491,7 +553,8 @@ frappe.ui.form.on('Opportunity Option', {
         var child8 = locals[cdt][cdn];
         child8.qty = 1;
         refresh_field("option_8");
-        frm.set_df_property('option_9', 'hidden', 0)
+        frm.set_df_property('option_9', 'hidden', 0);
+        frm.set_df_property('name9', 'hidden', 0);
         frm.set_df_property('option8_submit', 'hidden', 0);
         frm.set_df_property('option8_copy', 'hidden', 0);
 
@@ -501,7 +564,8 @@ frappe.ui.form.on('Opportunity Option', {
         var child9 = locals[cdt][cdn];
         child9.qty = 1;
         refresh_field("option_9");
-        frm.set_df_property('option_10', 'hidden', 0)
+        frm.set_df_property('option_10', 'hidden', 0);
+        frm.set_df_property('name10', 'hidden', 0);
         frm.set_df_property('option9_submit', 'hidden', 0);
         frm.set_df_property('option9_copy', 'hidden', 0);
 
@@ -678,4 +742,21 @@ cur_frm.cscript.item_code = function(doc, cdt, cdn) {
 	if (d.item_code) {
 		get_item_details(doc, d, cdt, cdn);
 	}
+}
+const set_css = (frm)=>{
+    for( var x=1; x<11 ; x++){
+        document.querySelectorAll("[data-fieldname='option_"+x+"']")[0].firstChild.style.display = "none";
+        document.querySelectorAll("[data-fieldname='name"+x+"']")[0].getElementsByTagName("label")[0].style.fontSize = 'large';
+        document.querySelectorAll("[data-fieldname='name"+x+"']")[0].getElementsByTagName("label")[0].style.fontWeight = 'bold';
+        document.querySelectorAll("[data-fieldname='name"+x+"']")[0].getElementsByTagName("input")[0].style.position = "absolute";
+        document.querySelectorAll("[data-fieldname='name"+x+"']")[0].getElementsByTagName("input")[0].style.top = "-40px";
+        document.querySelectorAll("[data-fieldname='name"+x+"']")[0].getElementsByTagName("input")[0].style.left = "100px";
+    }
+
+    for( var z=1; z<11 ; z++){
+        document.querySelectorAll("[data-fieldname='option"+z+"_submit']")[0].style.cssFloat = "left" ;
+        document.querySelectorAll("[data-fieldname='option"+z+"_cancel']")[0].style.cssFloat = "left";
+        document.querySelectorAll("[data-fieldname='option"+z+"_copy']")[0].style.cssFloat = "left";
+    }
+    
 }
