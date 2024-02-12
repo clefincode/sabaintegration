cc2103e1c9768a3c8c224696c1da05986f4bba9c
Ahmad Kamal Eddin <ahmad@clefincode.com>
2024-02-12 14:02:37 +0200

    Pre-Sales Engineer

---
+++ sabaintegration/sabaintegration/doctype/pre_sales_engineer/pre_sales_engineer.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Ahmad and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Pre-Sales Engineer', {
+	// refresh: function(frm) {
+
+	// }
+});
