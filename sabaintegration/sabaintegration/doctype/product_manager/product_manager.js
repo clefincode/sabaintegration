f4ba9cf6d424713256f724c9f23a9425c617e361
Ahmad Kamal Eddin <ahmad@clefincode.com>
2024-02-12 13:54:18 +0200

    Product Manager Doctype

---
+++ sabaintegration/sabaintegration/doctype/product_manager/product_manager.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Ahmad and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Product Manager', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ sabaintegration/sabaintegration/doctype/product_manager/product_manager.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2023, Ahmad and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class ProductManager(Document):
+	def validate(self):
+		self.set_position()
+
+	def set_position(self):
+		employee = get_employee(self)
+		if employee:
+			self.position = employee.position
+
+def get_employee(engineer):
+	if not engineer.employee: return
+	return frappe.get_doc("Employee", engineer.employee)
